a539e1deb6c24f8cf9ffad9d2ad5bce280711fb0
JUMPIP <jumpipgo@gmail.com>
2026-09-25 11:58:55 +0300

    Revert "style: redesign mobile home dashboard"
    This reverts commit bc53304865658c38b9f076e8a006908d5673bdef.

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -100,8 +100,7 @@ export function HomeScreen({ visible, onOpenDay, onOpenCycle, onToast, onCelebra
         </button>
       </header>
       <div id="homeBody">
-        <section className="hero home-overview">
-          <div className="overview-top"><span className="label">прогресс макроцикла</span><span className="overview-week">{completed ? 'завершено' : `неделя ${week} / 4`}</span></div>
+        <section className="hero">
           <div className="ring"><ProgressRing state={state} currentDay={currentDay} /> <div className="ring-c">{center}</div></div>
         </section>
 
